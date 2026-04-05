d38ad7142b22dba3aa9f5b995a9f563fc916b4ee
EngAhmedAbdullahiAl <atooremudug@gmail.com>
2026-04-05 10:19:22 -0700

    footer added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../layouts/Navbar'
 import AboutLayout from '../layouts/AboutLayout'
 import ProgressSection from '../layouts/ProgressSection'
+import Footer from '../layouts/Footer'
 const About = () => {
     return (
         <>
             <Navbar />            
             <AboutLayout />
             <ProgressSection/>
+            <Footer/>
             
         </>
     )
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../layouts/Navbar'
 import ServiceLayout from '../layouts/ServiceLayout'
+import Footer from '../layouts/Footer'
 const Service = () => {
     return (
         <>
             <Navbar />
             <ServiceLayout />
+            <Footer/>
         </>
     )
 }
